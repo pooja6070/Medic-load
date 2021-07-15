a1627883ebf20d0bb1407af2ca3f89040c812704
Pooja pokharel <pooja.pokhrel36@gmail.com>
2021-07-15 14:38:25 +0545

    user bill api data

---
+++ app/src/main/java/com/android/pharmacy/bll/LoginBLL.java
@@ -0,0 +1,35 @@
+package com.android.pharmacy.bll;
+
+import com.android.pharmacy.api.UsersAPI;
+import com.android.pharmacy.serverresponse.SignUpResponse;
+import com.android.pharmacy.url.Url;
+
+import java.io.IOException;
+
+import retrofit2.Call;
+import retrofit2.Response;
+
+public class LoginBLL {
+
+    boolean isSuccess = false;
+
+    public boolean checkUser(String username, String password) {
+
+        UsersAPI usersAPI = Url.getInstance().create(UsersAPI.class);
+        Call<SignUpResponse> usersCall = usersAPI.checkUser(username, password);
+
+        try {
+            Response<SignUpResponse> loginResponse = usersCall.execute();
+            if (loginResponse.isSuccessful() &&
+                    loginResponse.body().getStatus().equals("Login success!")) {
+
+                Url.token += loginResponse.body().getToken();
+                // Url.Cookie = imageResponseResponse.headers().get("Set-Cookie");
+                isSuccess = true;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return isSuccess;
+    }
+}
